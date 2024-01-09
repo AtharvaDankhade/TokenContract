7e3bd91a34b7eed34081931d6e67da89e76b03e5
Atharva Dankhade <134969000+AtharvaDankhade@users.noreply.github.com>
2024-01-09 16:31:42 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -4,54 +4,47 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RamMint is ERC20("RamMint","RR"), Ownable {
-    uint _start;
-    uint _end;
-    struct holder_info {
-        uint token_quantity;
-        uint locking_time;
+contract RamMint is ERC20, Ownable {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _token[_symbol] = true;
+        Admins[msg.sender] = true;
     }
 
-    mapping (address => holder_info) public token_holders;
+    mapping(string => bool) private _token;
+    mapping (address => bool) private Admins;
 
-    //Checks if the token time span is expired or not, if expired then the token is taken back from the specicied account.
-    modifier timeOver() {
-        if(block.timestamp >= token_holders[msg.sender].locking_time)
-        {
-            mintForContract(token_holders[msg.sender].token_quantity);
-            token_holders[msg.sender].token_quantity = 0;
-            token_holders[msg.sender].locking_time = 0;
-        }
+    modifier checkAdmins() {
+        require(Admins[msg.sender] == true,"Not Authorised user");
         _;
     }
 
-    //Initializes the contract with the required number of tokens.
-    function mintForContract(uint _tokensToMint) public onlyOwner {
-        _mint(msg.sender,_tokensToMint);
+    //It will add new admins in the system.
+    function addAdmins(address _address) public onlyOwner { 
+        Admins[_address] = true;
     }
 
-    //adds addresses with the specified token quantity and time span.
-    function addMembers(address _holder_address, uint _token_quantity, uint _locking_time)  public onlyOwner{
-        require(balanceOf(msg.sender) > _token_quantity ,"Not enough tokens to add new member");
-        _transfer(msg.sender, _holder_address, _token_quantity);
-        lock(_holder_address,_locking_time);
-        token_holders[_holder_address].token_quantity = _token_quantity;
+    //It will lock the token.
+    function lockTokens(string memory _symbol) public checkAdmins {
+        _token[_symbol] = true;
     }
 
-    //locks the token for specified amount of time.
-    function lock(address _address,uint _time) internal {
-        _start = block.timestamp;
-        _end = _time + _start;
-        token_holders[_address].locking_time = _end;
+    //It will unlock the token.
+    function unlockToken(string memory _symbol) public checkAdmins {
+        _token[_symbol] = false;
     }
 
-    //Checks if there is token availabel or not by checking the expiration of the token.
-    function getTimeLeft() public timeOver returns(uint) {
-        return token_holders[msg.sender].locking_time - block.timestamp;
+    //only authorized address can access the function and mint token for any address.
+    function unlockAndClame(string memory _symbol,address _to, uint _quantity) public checkAdmins
+    {
+        require(_token[_symbol] == true,"Token is locked");
+        _mint(_to, _quantity);
     }
 
-    function spendToken() public timeOver {
-        //Any functionality which uses tokens.
+    function removeAdmin(address _address) public {
+        Admins[_address] = false;
     }
 
+    function isTokenLocked(string memory symbol) external view returns (bool) {
+        return _token[symbol];
+    }
 }
