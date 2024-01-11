4ad4abc80c91d4ad9390302de3bdda3f3788937c
Atharva Dankhade <134969000+AtharvaDankhade@users.noreply.github.com>
2024-01-11 16:01:09 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,50 +1,82 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RamMint is ERC20, Ownable {
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
-        _token[_symbol] = true;
-        Admins[msg.sender] = true;
+contract WhitelistedToken is ERC20, Ownable {
+    address[] private _whitelistedAccounts;
+    bool private _isTransferLocked;
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) Ownable() {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
     }
 
-    mapping(string => bool) private _token;
-    mapping (address => bool) private Admins;
+    function distributeToWhitelist(uint256 amount) external onlyOwner {
+        require(_whitelistedAccounts.length > 0, "No whitelisted accounts provided");
+        require(amount > 0, "Amount must be greater than 0");
+        require(!_isTransferLocked, "Token transfer is currently locked");
 
-    modifier checkAdmins() {
-        require(Admins[msg.sender] == true,"Not Authorised user");
-        _;
+        uint256 share = amount / _whitelistedAccounts.length;
+        for (uint256 i = 0; i < _whitelistedAccounts.length; i++) {
+            address recipient = _whitelistedAccounts[i];
+            _transfer(owner(), recipient, share);
+        }
     }
 
-    //It will add new admins in the system.
-    function addAdmins(address _address) public onlyOwner { 
-        Admins[_address] = true;
+    // Owner-only functions to manage whitelisted accounts
+    function whitelistAccount(address account) external onlyOwner {
+        require(!_isWhitelisted(account), "Account already whitelisted");
+        _whitelistedAccounts.push(account);
     }
 
-    //It will lock the token.
-    function lockTokens(string memory _symbol) public checkAdmins {
-        _token[_symbol] = true;
+    function unwhitelistAccount(address account) external onlyOwner {
+        require(_isWhitelisted(account), "Account not whitelisted");
+        _removeWhitelistedAccount(account);
     }
 
-    //It will unlock the token.
-    function unlockToken(string memory _symbol) public checkAdmins {
-        _token[_symbol] = false;
+    function getWhitelistedAccounts() external view returns (address[] memory) {
+        return _whitelistedAccounts;
     }
 
-    //only authorized address can access the function and mint token for any address.
-    function unlockAndClame(string memory _symbol,address _to, uint _quantity) public checkAdmins
-    {
-        require(_token[_symbol] == true,"Token is locked");
-        _mint(_to, _quantity);
+    function lockTransfer() external onlyOwner {
+        _isTransferLocked = true;
     }
 
-    function removeAdmin(address _address) public {
-        Admins[_address] = false;
+    function unlockTransfer() external onlyOwner {
+        _isTransferLocked = false;
     }
 
-    function isTokenLocked(string memory symbol) external view returns (bool) {
-        return _token[symbol];
+    function transfer(address to, uint256 value) public override returns (bool) {
+        require(!_isTransferLocked, "Token transfer is currently locked");
+        return super.transfer(to, value);
+    }
+
+    function transferFrom(address from, address to, uint256 value) public override returns (bool) {
+        require(!_isTransferLocked, "Token transfer is currently locked");
+        return super.transferFrom(from, to, value);
+    }
+
+    function _isWhitelisted(address account) internal view returns (bool) {
+        for (uint256 i = 0; i < _whitelistedAccounts.length; i++) {
+            if (_whitelistedAccounts[i] == account) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function _removeWhitelistedAccount(address account) internal {
+        for (uint256 i = 0; i < _whitelistedAccounts.length; i++) {
+            if (_whitelistedAccounts[i] == account) {
+                if (i != _whitelistedAccounts.length - 1) {
+                    _whitelistedAccounts[i] = _whitelistedAccounts[_whitelistedAccounts.length - 1];
+                }
+                _whitelistedAccounts.pop();
+                break;
+            }
+        }
     }
 }
